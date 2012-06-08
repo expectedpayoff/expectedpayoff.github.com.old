107232cd52f12586ea5c332ab9956bfe834fdab5
Byron Gibson <fbg111@gmail.com>
2012-06-08 01:18:56 -0700

    update

---
+++ README.sh
@@ -0,0 +1,15 @@
+
+### Deploy
+
+To deploy, run rake_post.sh or
+
+`$> rake generate`
+`$> rake deploy`
+
+### Backup
+
+To push the entire blog framework to github repo:
+
+`$> git add -A`
+`$> git commit -m 'update'`
+`$> git push source source`
