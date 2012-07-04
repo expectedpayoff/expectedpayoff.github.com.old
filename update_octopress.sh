fd74b69b4e8abe72ec2f672ce9f3a4f561196a7c
Byron Gibson <fbg111@gmail.com>
2012-07-04 15:46:48 -0700

    octopress update

--- update_octopress.sh
+++ update_octopress.sh
@@ -2,8 +2,8 @@
 
 # http://octopress.org/docs/updating/
 
-#git pull octopress master     # Get the latest Octopress
-git pull upstream master     # Get the latest Octopress
+git pull octopress master     # Get the latest Octopress
+#git pull upstream master     # Get the latest Octopress
 bundle install                # Keep gems updated
 rake update_source            # update the template's source
 rake update_style             # update the template's style
